7cdcd40149a276d63aed2625b64e0b17970265d1
Yahya <yahya@berlin.universe.io>
2017-12-14 15:20:20 +0100

    add all files

--- gulpfile.js
+++ gulpfile.js
@@ -9,18 +9,27 @@ var uglify = require('gulp-uglify');
 var pkg = require('./package.json');
 
 // Set the banner content
-var banner = ['/*!\n',
-    ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-    ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-    ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
-    ' */\n',
-    ''
-].join('');
+// var banner = ['/*!\n',
+//     ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
+//     ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
+//     ' * Licensed under <%= pkg.license.type %> (<%= pkg.license.url %>)\n',
+//     ' */\n',
+//     ''
+// ].join('');
 
 // Compile LESS files from /less into /css
-// gulp.task('less', function() {
-//     return gulp.src('less/agency.less')
-//         .pipe(less())
+gulp.task('less', function() {
+    return gulp.src('less/agency.less')
+        .pipe(less())
+        .pipe(gulp.dest('css'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
+});
+
+// gulp.task('sass', function() {
+//     return gulp.src('scss/agency.scss')
+//         .pipe(sass())
 //         .pipe(header(banner, { pkg: pkg }))
 //         .pipe(gulp.dest('css'))
 //         .pipe(browserSync.reload({
@@ -28,18 +37,8 @@ var banner = ['/*!\n',
 //         }))
 // });
 
-gulp.task('sass', function() {
-    return gulp.src('scss/agency.scss')
-        .pipe(sass())
-        .pipe(header(banner, { pkg: pkg }))
-        .pipe(gulp.dest('css'))
-        .pipe(browserSync.reload({
-            stream: true
-        }))
-});
-
 // Minify compiled CSS
-gulp.task('minify-css', ['sass'], function() {
+gulp.task('minify-css', ['less'], function() {
     return gulp.src('css/agency.css')
         .pipe(cleanCSS({ compatibility: 'ie8' }))
         .pipe(rename({ suffix: '.min' }))
@@ -53,7 +52,6 @@ gulp.task('minify-css', ['sass'], function() {
 gulp.task('minify-js', function() {
     return gulp.src('js/agency.js')
         .pipe(uglify())
-        .pipe(header(banner, { pkg: pkg }))
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest('js'))
         .pipe(browserSync.reload({
@@ -81,7 +79,7 @@ gulp.task('copy', function() {
 })
 
 // Run everything
-gulp.task('default', ['sass', 'minify-css', 'minify-js', 'copy']);
+gulp.task('default', ['minify-css', 'less', 'minify-js', 'copy']);
 
 // Configure the browserSync task
 gulp.task('browserSync', function() {
@@ -93,8 +91,8 @@ gulp.task('browserSync', function() {
 })
 
 // Dev task with browserSync
-gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js'], function() {
-    gulp.watch('scss/*.scss', ['sass']);
+gulp.task('dev', ['browserSync', 'less', 'minify-css', 'minify-js'], function() {
+    gulp.watch('less/*.less', ['less']);
     gulp.watch('css/*.css', ['minify-css']);
     gulp.watch('js/*.js', ['minify-js']);
     // Reloads the browser whenever HTML or JS files change
@@ -104,4 +102,3 @@ gulp.task('dev', ['browserSync', 'sass', 'minify-css', 'minify-js'], function() 
 
 // Compiles SCSS files from /scss into /css
 // NOTE: This theme uses LESS by default. To swtich to SCSS you will need to update this gulpfile by changing the 'less' tasks to run 'sass'!
-
